8d906d9e23ac9aa074bc54d123430ecc6f0bb9b2
Xintong Jiang <231744781+xintong-J@users.noreply.github.com>
2025-10-30 01:24:18 +0000

    make the option numbers bold and options blue

--- src/app/todo.py
+++ src/app/todo.py
@@ -1,6 +1,7 @@
 import json
 import os
 from importlib import resources
+from rich import print
 
 TASKS_FILE = resources.files("app.data").joinpath("tasks.json")
 
@@ -76,12 +77,12 @@ def main():
     tasks = load_tasks()
 
     while True:
-        print("\n==== To-Do List Menu ====")
-        print("1. View tasks")
-        print("2. Add a task")
-        print("3. Mark task as complete")
-        print("4. Delete a task")
-        print("5. Exit")
+        print("\n[bold magenta]==== To-Do List Menu ====[/bold magenta]")
+        print("[blue][bold]1.[/bold] View tasks[/blue]")
+        print("[blue][bold]2.[/bold] Add a task[/blue]")
+        print("[blue][bold]3.[/bold] Mark task as complete[/blue]")
+        print("[blue][bold]4.[/bold] Delete a task[/blue]")
+        print("[blue][bold]5.[/bold] Exit[/blue]")
 
         choice = input("Select an option (1-5): ").strip()
 
